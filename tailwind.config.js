95d055423877e27c67626c096bf55a9d46918741
Abiyan Azis <abiyanamirulazis017@gmail.com>
2026-09-18 15:56:40 +0700

    Upload portofolio pertama

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./projects.html",
+    "./contact.html",
+    "./js-task/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
