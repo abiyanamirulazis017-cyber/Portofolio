ba2becb15befbb61f7cc999aaab971c2f46a813c
Abiyan Azis <abiyanamirulazis017@gmail.com>
2026-09-18 18:41:23 +0700

    update: hero dan tentang saya

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,15 @@ module.exports = {
     "./index.html",
     "./projects.html",
     "./contact.html",
-    "./js-task/**/*.html"
+    "./js-task/**/*.html",
+    "./assets/js/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Interr', 'ui-sans', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
